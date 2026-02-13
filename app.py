24082e5bdfb5bdcc7054caea4a656f56a8fa01ee
Lance Gong <xzdxrz666@gmail.com>
2026-02-14 01:32:37 +1100

    fix cache logic (#2)

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ APP_HOST = "127.0.0.1"
 APP_PORT = 5000
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
+UPLOAD_DIR = os.path.join(BASE_DIR, ".cache/uploads")
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 ALLOWED_EXT = {".png", ".jpg", ".jpeg", ".webp"}
